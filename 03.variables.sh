6ad1bb47609bf4aa6401bf86526dc6ded8c4376f
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-29 12:22:47 +0530

    conversation

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: Hello $person2. How are you?"
+echo "$person2: I'm fine . Thanks for asking. How do you do $person1?"
+echo "$person1: I'm doin great. What are you learning?"
+echo "$person2: Im learnin shell."
