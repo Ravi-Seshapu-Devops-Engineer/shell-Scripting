bdbcc9a06f8c370b8a72f15e0126210fa8b01f99
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-29 12:03:36 +0530

    conversation

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hello ramesh. How are you?"
+echo "Ramesh: I'm fine . Thanks for asking. How do you do Suresh?"
+echo "Suresh: I'm doin great. What are you learning?"
+echo "Ramesh: Im learnin shell.
