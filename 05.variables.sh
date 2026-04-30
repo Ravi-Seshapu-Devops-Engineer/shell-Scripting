251e77e526abab95d6afc728a30b709a02d19a6a
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 11:01:46 +0530

    committed something in variables file

--- 05.variables.sh
+++ 05.variables.sh
@@ -2,4 +2,13 @@
 
 TIME_STAMP=$(date)
 
-echo the code is executed at $TIME_STAMP
+echo the code is executed at $TIME_STAMP"
+
+START_TIME=$(date +%s)
+sleep 10
+
+END_TIME=$(date +%s)
+
+Total_TIME=$(($END_TIME - $START_TIME))
+
+echo "Script executed in $Total_TIME"
