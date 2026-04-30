268f76c15bba344d886d798d884b9dfe40b5c487
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 17:06:18 +0530

    committed something in logs file

--- 11.logs.sh
+++ 11.logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-  echo "run the script with root user access"
+  echo "run the script with root user access" | tee -a $LOGS_FILE
   exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 validate(){
   if [ $1 -ne 0 ]; then
-    echo "$2 is failed"
+    echo "$2 is failed" | tee -a $LOGS_FILE
     exit 1
   else
-    echo "$2 is Success"
+    echo "$2 is Success" | tee -a $LOGS_FILE
   fi
 }
 
