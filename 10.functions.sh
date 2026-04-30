73dea879eebe01edba774a1f86dbb5c7043e9f1c
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 16:28:22 +0530

    committed something in installation file

---
+++ 10.functions.sh
@@ -0,0 +1,26 @@
+#!/bin bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "run the script with root user access"
+  exit 1
+fi
+
+validate(){
+  if [ $1 -ne 0 ]; then
+    echo "$2 is failed"
+    exit 1
+  else
+    echo "$2 is Success"
+}
+
+
+dnf install nginx -y
+validate $? "installing nginx"
+
+dnf install mysql -y
+validate $? "installing nginx"
+
+dnf install nodejs -y
+validate $? "installing nginx"
