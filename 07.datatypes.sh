52f0ea4423d5b36076786864ee11cadc99983a6b
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 15:19:36 +0530

    committed something in variables6 file

--- 07.datatypes.sh
+++ 07.datatypes.sh
@@ -7,3 +7,12 @@ Num3=400
 SUM=$(($Num1+$Num2+$Num3))
 
 echo "sum is $SUM"
+
+#Array
+
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "Fruits are ${FRUITS[@]}"
+echo "First Fruit is ${FRUITS[0]}"
+echo "second Fruit is ${FRUITS[1]}"
+echo "Third Fruit is ${FRUITS[2]}"
