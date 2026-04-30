bd34a32dfe7e2dd4bac88230b7220433f64d9556
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 14:55:26 +0530

    committed something in variables6 file

---
+++ 06.variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All arguments passes to the script $@"
+echo "No of variables passed to the script $#"
+echo "Present working directory $PWD"
+echo "Script name $0"
+echo "Who is running the script $USER"
+echo "Home directory of the curent user $HOME"
+echo "PID odf the script $$"
+sleep 100
+echo "PID of the recently executed background process $!"
+echo "all arguments passed to the script $*"
