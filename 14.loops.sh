f29c3d49979deafa37aa6e026d4a2e853e8246c2
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-04-30 17:38:33 +0530

    committed something in 13loops file

---
+++ 14.loops.sh
@@ -0,0 +1,44 @@
+#!/bin bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+  echo "run the script with root user access" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+validate(){
+  if [ $1 -ne 0 ]; then
+    echo "$2 is failed" | tee -a $LOGS_FILE
+    exit 1
+  else
+    echo "$2 is Success" | tee -a $LOGS_FILE
+  fi
+}
+
+
+# dnf install nginx -y &>> $LOGS_FILE
+# validate $? "installing nginx"
+
+# dnf install mysql -y &>> $LOGS_FILE
+# validate $? "installing mysql"
+
+# dnf install nodejs -y &>> $LOGS_FILE
+# validate $? "installing nodejs"
+
+for package in $@  # sudo sh 13.loops.sh nginx nodejshtml( passing the aruments wile running the script)
+do
+  dnf list installed $package &>> LOGS_FILE
+  if [ $? -ne 0 ]; then
+    echo "$package is not installed, installing now"
+    dnf install $package -y &>> $LOGS_FILE
+    validate $? "installing $package"
+  else
+    echo "$package is already installed, skipping"
+  fi
+
+done
