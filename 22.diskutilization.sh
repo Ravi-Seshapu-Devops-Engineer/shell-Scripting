183ddf700904ef78134f8965699fcb728ba613b5
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-05 20:19:01 +0530

    actions

--- 22.diskutilization.sh
+++ 22.diskutilization.sh
@@ -6,6 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 MESSAGE=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
   echo -e "$(date "+%Y-%m-%d-%H-%M-%S") | $1" | tee -a $LOGS_FILE
@@ -26,4 +27,7 @@ while IFS= read -r line
   done <<< "$Disk_Usage"
 
 echo -e "$MESSAGE"
+
+sh mail.sh "ravishankarfrom1998@gmail.com" "High disk usage alert on $IP_ADDRESS" "$MESSAGE" "High disk usage alert" "$IP_ADDRESS" "TO DEVOPS TEAM"
+
 
