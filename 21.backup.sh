2a1f680676c12a9a8a399f2790ffddbac5ab5a4c
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-05 15:03:52 +0530

    actions

---
+++ 21.backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/lo/shell-Script/"
+LOGS_FILE="/var/log/shell-Script/backup.log"
+
+
+if [ ! -d $LOGS_FOLDER ]; then
+  echo "$LOGS_FOLDER does not exist"
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+usage(){
+  echo -e "$R Usage backup from to <SOURCE_DIR> <DEST_DIR> <DAYS> [default 14 days] $N "
+  exit 1
+}
+
+if [ $# -lt 2 ]; then
+  usage
+fi
