253bdd7ca09dd5f3e31cc03a74a4dc474c67dff4
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-05 19:24:39 +0530

    actions

---
+++ 22.diskutilization.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MESSAGE=""
+
+log(){
+  echo -e "$(date "+%Y-%m-%d-%H-%M-%S") | $1" | tee -a $LOGS_FILE
+}
+
+Disk_Usage=$(df -hT|grep -v Filesystem)
+Usage_Threshold=3
+
+while IFS=read -r line
+do
+  usage=$(df -hT |grep -v Filesystem | awk '{print $6}' | cut -d % -f1)
+  partition=$(df -hT |grep -v Filesystem | awk '{print $7}')
+
+  if [ "$usage" -gt "$Usage_Threshold" ]; then
+    MESSAGE+="High disk usage on $partition:$usage"
+  fi
+
+done <<< $Disk_Usage
+
+echo "$MESSAGE"
+
