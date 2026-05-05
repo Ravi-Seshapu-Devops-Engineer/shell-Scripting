f31b269a2b6cbfe95ed498429d6d98df33b6149e
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-05 21:07:19 +0530

    actions

--- 22.diskutilization.sh
+++ 22.diskutilization.sh
@@ -28,6 +28,6 @@ while IFS= read -r line
 
 echo -e "$MESSAGE"
 
-sh mail.sh "ravishankarfrom1998@gmail.com" "High disk usage alert on $IP_ADDRESS" "$MESSAGE" "High disk usage alert" "$IP_ADDRESS" "TO DEVOPS TEAM"
+sh 23.mail.sh "ravishankarfrom1998@gmail.com" "High disk usage alert on $IP_ADDRESS" "$MESSAGE" "High disk usage alert" "$IP_ADDRESS" "TO DEVOPS TEAM"
 
 
