e12b20997e9f7f06b91b526557cbc2bc4c2ff230
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-05-05 15:45:04 +0530

    actions

--- 21.backup.sh
+++ 21.backup.sh
@@ -4,15 +4,17 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3: -14}
 LOGS_FOLDER="/var/log/shell-Scripting/"
 LOGS_FILE="/var/log/shell-Scripting/backup.log"
 
-
-if [ ! -d $LOGS_FOLDER ]; then
-  echo "$LOGS_FOLDER does not exist"
+if [ $USERID -ne 0 ]; then
+  echo -e "$R run the script with root user access $N" | tee -a $LOGS_FILE
   exit 1
 fi
+
 
 mkdir -p $LOGS_FOLDER
 
@@ -21,6 +23,28 @@ usage(){
   exit 1
 }
 
+log(){
+  echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
 if [ $# -lt 2 ]; then
   usage
 fi
+
+if [ ! -d $SOURCE_DIR ]; then
+  echo "$R Source directory $SOURCE_DIR $N does not exist"
+  exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+  echo "$R Destination directory $DEST_DIR $N does not exist"
+  exit 1
+fi
+
+#find the files
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS)
+
+log "BACKUP started"
+log "Source directory: $SOURCE_DIR"
+log "Dest dir :$DEST_DIR"
+log "Days: $DAYS
